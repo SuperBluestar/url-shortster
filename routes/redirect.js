4e35018a222e331534eff9692851524cf9518e9c
SuperBluestar <alexander116gm@gmail.com>
2022-05-08 07:15:47 +0900

    add redirect feature

---
+++ routes/redirect.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const router = express.Router();
+
+const Url = require('../models/url');
+
+router.get('/:shortcode', async (req, res) => {
+  try {
+    const url = await Url.findOne({
+      urlCode: req.params.shortcode,
+    });
+    if (url) {
+      return res.redirect(url.urlOriginal);
+    } else {
+      return res.status(404).json('No URL Found');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json('Server Error');
+  }
+});
+
+module.exports = router;
