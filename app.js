9e3c67492ee4aa5ac2ba4ae144204140e664bc22
SuperBluestar <alexander116gm@gmail.com>
2022-05-08 18:11:25 +0900

    fix cors in backend

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 require('dotenv').config();
 
@@ -15,6 +16,8 @@ app.use(
   })
 );
 app.use(bodyParser.json()); // parse req.body
+
+app.use(cors());
 
 app.get('/api-testing', (req, res) => {
   res.json({
